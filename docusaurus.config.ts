051a4f292e7fb6aebd5d3e878be6f6c8debf572e
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2025-02-28 16:35:08 +0530

    remove Community and About links from the navigation configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,8 +89,8 @@ const config: Config = {
 				},
 
 				{to: "/blog", label: "Blog", position: "left"},
-				{to: "/community", label: "Community", position: "left"},
-				{to: "/about", label: "About", position: "left"},
+				// {to: "/community", label: "Community", position: "left"},
+				// {to: "/about", label: "About", position: "left"},
 			],
 		},
 		footer: {
